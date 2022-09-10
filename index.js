25e37888588814768874e50ab28ea45eb6c144ce
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-10 16:48:04 +0300

    open and close popup

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 let dog_image = document.getElementById("dog-image")
 const image_url = `https://dog.ceo/api/breeds/image/random`
 
+const popup = document.getElementById("open")
+const close_btn = document.getElementById("close")
+
 //Fetch Dog Image
 fetch(image_url).then((response) => response.json())
     .then((data) => {
         let img = data['message']
         dog_image.src = img
     })
+
+// Open PopUp to Display the Results
+const openPopup = () => {
+    popup.classList.add('open-popup')
+}
+
+// Open PopUp
+const closePopup = () => {
+    popup.classList.remove('open-popup')
+}
+
+predict_btn.addEventListener('click', openPopup)
+close_btn.addEventListener('click', closePopup)
